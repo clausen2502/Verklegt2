eac23b53acf41e31dbe077973542218fa6ab75a9
elfasifh <elfa23@ru.is>
2025-05-07 11:52:01 +0000

    offer edited

--- offer/views.py
+++ offer/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import PurchaseOffer
-from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     return HttpResponse(f"Response from {request.path}")
