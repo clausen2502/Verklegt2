74604ccc176646a933b880b1da816d4e08fb8684
matthildurmj <matthildurj23@ru.is>
2025-05-08 13:29:13 +0000

    JS fyrir type filter

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,23 @@
 
-/*Filter JavaScript Location ---------------------------------------------------------------------------------------------------------------*/
+/*Location Filter JavaScript  ---------------------------------------------------------------------------------------------------------------*/
 function toggleLocationDropdown() {
     const dropdown = document.getElementById('locationDropdown');
     if (dropdown.style.display === 'none') {
     dropdown.style.display = 'block';
-} else {
-    dropdown.style.display = 'none';
+    } else {
+        dropdown.style.display = 'none';
+    }
 }
+
+/*Type Filter JavaScript  ---------------------------------------------------------------------------------------------------------------*/
+function toggleTypeDropdown() {
+    const typeDropdown = document.getElementById('type-dropdown');
+    if (typeDropdown.style.display === 'none' || typeDropdown.style.display === '') {
+        typeDropdown.style.display = 'block';
+    } else {
+        typeDropdown.style.display = 'none';
+    }
 }
+
+
+
