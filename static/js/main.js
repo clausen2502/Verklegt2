8e68e942628304a87113adc46fc02f252d06d001
matthildurmj <matthildurj23@ru.is>
2025-05-08 13:12:29 +0000

    Javascript fyrir location filter

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,10 @@
 
+/*Filter JavaScript Location ---------------------------------------------------------------------------------------------------------------*/
+function toggleLocationDropdown() {
+    const dropdown = document.getElementById('locationDropdown');
+    if (dropdown.style.display === 'none') {
+    dropdown.style.display = 'block';
+} else {
+    dropdown.style.display = 'none';
+}
+}
