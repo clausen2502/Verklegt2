2c6c2f489647eee2cfd0a581ab3cca6401540dcb
elfasifh <elfa23@ru.is>
2025-05-08 13:10:51 +0000

    bank transfer

--- finalization/urls.py
+++ finalization/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('<int:offer_id>/', views.finalize_contact_view, name='finalize-offer'),
     path('<int:offer_id>/payment/', views.finalize_payment_view, name='finalize-payment'),
+    path("finalize/<int:offer_id>/payment/card/", views.credit_card_form, name="credit-card-form"),
+    path("finalize/<int:offer_id>/payment/bank/", views.bank_transfer_form, name="bank-transfer-form"),
+    path("finalize/<int:offer_id>/payment/mortgage/", views.mortgage_form, name="mortgage-form"),
 ]
