712b786c7a60806b1f2180e55dc83c0516569096
Isak Clausen <isakb23@ru.is>
2025-05-11 12:52:20 +0000

    user.html

--- property/views.py
+++ property/views.py
@@ -6,15 +6,77 @@ from django.contrib import messages
 from user.models import SellerUser
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
-
+from django.db.models import Q
 
 def index(request):
+    """Display a filtered list of properties based on user-selected criteria."""
+
     properties = Property.objects.prefetch_related('photos').all()
-    return render(request, "property/properties.html", {
+
+    # Extract filter values
+    query = request.GET.get("q")
+    size_from = request.GET.get("size_from")
+    size_to = request.GET.get("size_to")
+    price_from = request.GET.get("price_from")
+    price_to = request.GET.get("price_to")
+    types = request.GET.getlist("type")
+    locations = request.GET.getlist("location")
+    bedrooms = request.GET.get("bedrooms")
+    bathrooms = request.GET.get("bathrooms")
+
+    # Apply filters
+    if query:
+        properties = properties.filter(
+            Q(street_name__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+    if size_from:
+        properties = properties.filter(square_meters__gte=size_from)
+    if size_to:
+        properties = properties.filter(square_meters__lte=size_to)
+
+    if price_from:
+        properties = properties.filter(price__gte=price_from)
+    if price_to:
+        properties = properties.filter(price__lte=price_to)
+
+    if types:
+        properties = properties.filter(type__in=types)
+
+    if locations:
+        properties = properties.filter(postal_code__in=locations)
+
+    if bedrooms:
+        properties = properties.filter(bedrooms__gte=bedrooms)
+
+    if bathrooms:
+        properties = properties.filter(bathrooms__gte=bathrooms)
+
+    # Choice sets for form rendering
+    type_choices = [
+        ("apartment", "Apartment"),
+        ("house", "House"),
+        ("studio", "Studio"),
+        ("other", "Other"),
+    ]
+    size_choices = ["0", "40", "60", "70", "80", "90", "100", "115", "130"]
+    room_choices = ["1", "2", "3", "4", "5"]
+    price_choices = [
+        "1000000", "5000000", "10000000", "15000000", "20000000",
+        "25000000", "30000000", "40000000", "50000000", "60000000",
+        "75000000", "100000000", "150000000"
+    ]
+
+    return render(request, "user/user.html", {
         "properties": properties,
-        'property_count': properties.count()
+        "property_count": properties.count(),
+        "type_choices": type_choices,
+        "size_choices": size_choices,
+        "room_choices": room_choices,
+        "price_choices": price_choices,
     })
+
 
 def get_property_by_id(request, id):
     property = Property.objects.prefetch_related('photos').get(id=id)
