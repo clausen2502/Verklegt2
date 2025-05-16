5f4dbbddafb17acfbfcd936b8bc7e9f020850641
Isak Clausen <isakb23@ru.is>
2025-05-16 11:29:53 +0000

    manager-delete-property

--- manager/urls.py
+++ manager/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.admin_dashboard, name='admin-dashboard'),  # ← for /manager/
     path('dashboard/', views.admin_dashboard, name='admin-dashboard'),
     path('delete-user/<int:user_id>/', views.delete_user, name='delete-user'),
-    path('delete-property/<int:property_id>/', views.delete_property, name='delete-property'),
+    path('manager/delete-property/<int:property_id>/', views.delete_property, name='manager-delete-property'),
+
 ]
